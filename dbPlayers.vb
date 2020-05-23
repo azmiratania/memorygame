95f9f2388c8601db550ba0ea6f790add4f221ab6
Azmira Tania <azmiratania@users.noreply.github.com>
2020-05-23 22:39:36 +0800

    Update dbPlayers.vb

--- dbPlayers.vb
+++ dbPlayers.vb
@@ -1,12 +1,3 @@
-﻿'*********************************
-'  Azmira Tania          (0839408) 
-'  Nur Shahirah Roslan   (0955829)
-'  VISUAL BASIC PROGRAMMING  
-'  ASSIGNMENT (2009/2010 S2)
-'  Class:DIT/FT/1B01 
-'*********************************
-
-
 Imports System.Data.SqlClient
 
 Public Class dbPlayers
