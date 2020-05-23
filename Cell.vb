7e5e07113573737054ea7f637db5a1e2720f390f
Azmira Tania <azmiratania@users.noreply.github.com>
2020-05-23 22:36:58 +0800

    Update Cell.vb

--- Cell.vb
+++ Cell.vb
@@ -1,11 +1,3 @@
-'*********************************
-'  Azmira Tania          (0839408) 
-'  Nur Shahirah Roslan   (0955829)
-'  VISUAL BASIC PROGRAMMING  
-'  ASSIGNMENT (2009/2010 S2)
-'  Class:DIT/FT/1B01 
-'*********************************
-
 Public Class Cell
 
     Private mFileName As String
