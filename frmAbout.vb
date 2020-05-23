b5a1bd37f338acfb6695480d26f79b306b68e47a
Azmira Tania <azmiratania@users.noreply.github.com>
2020-05-23 22:39:03 +0800

    Update frmAbout.vb

--- frmAbout.vb
+++ frmAbout.vb
@@ -1,11 +1,3 @@
-﻿'*********************************
-'  Azmira Tania          (0839408) 
-'  Nur Shahirah Roslan   (0955829)
-'  VISUAL BASIC PROGRAMMING  
-'  ASSIGNMENT (2009/2010 S2)
-'  Class:DIT/FT/1B01 
-'*********************************
-
 Public Class frmAbout
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
